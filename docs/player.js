5a00667453929d16b776a27080696d7f17223d82
natunatta <s16009ha@sfc.keio.ac.jp>
2017-04-27 12:39:10 +0900

    表か裏かマンに勝つコード追加

--- docs/player.js
+++ docs/player.js
@@ -1,3 +1,17 @@
+const GU = 0;
+const CHOKI = 1;
+const PA =2;
+
+let matches = 0;
 function action(oppornent){
-  return 1;
+  matches = matches + 1;
+  const amari = matches %2; 
+  const isEven = amari ==0; //Trueは試合数が偶数
+  //console.log(amari); //(amari)偶数か奇数かわかる
+  console.log(`${matches} % 2 = ${amari} : ${isEven}`);
+  if(isEven){//Trueの時(偶数の時)はGUを返す
+    return GU;
+  }
+  return PA;
 }
+
